76c9503bd94c133a2756ab57221ee77ea6062ac5
CarissaThomas <carissaillyy@gmail.com>
2019-12-11 11:28:41 -0800

    css grid rewrite complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from "@angular/router";
 
 
 @Component({
@@ -10,4 +10,59 @@ import { Component } from '@angular/core';
 
 export class AppComponent  {
   title = 'Flight PDX';
+  imageSrc: string;
+  isImageDisplayed: boolean;
+
+  constructor(public router: Router){
+    this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) { 
+        this.getImageLocation();
+      }
+    });
+  }
+
+  getImageLocation() {
+    switch(this.router.url) { 
+      case '/events': { 
+        this.imageSrc = "../../assets/moduleImages/EventsLogo.png";
+        this.isImageDisplayed = true;
+         break; 
+      } 
+
+      case '/about': { 
+        this.imageSrc = "../../assets/moduleImages/pressedoutsmilie.png";
+        this.isImageDisplayed = true;
+         break; 
+      } 
+
+      case '/artists': { 
+        this.isImageDisplayed = false;
+         break; 
+      } 
+
+      case '/': { 
+        this.isImageDisplayed = false;
+         break; 
+      } 
+
+      case '/links': { 
+        this.imageSrc = "../../assets/moduleImages/linkicon.png";
+        this.isImageDisplayed = true;
+         break; 
+      } 
+
+      case '/recordlabel': { 
+        this.imageSrc = "../../assets/moduleImages/record-label.png";
+        this.isImageDisplayed = true;
+         break; 
+      } 
+      default: { 
+         //statements; 
+         break; 
+      } 
+   } 
+  }
+
 }
+
+
